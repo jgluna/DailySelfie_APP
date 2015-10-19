66df909c75f5895d39e98c76426fbdee51f1418a
jgluna <jgluna85@gmail.com>
2015-10-19 17:29:15 -0500

    Added notification on Alarm receiver

--- app/src/main/java/com/github/jgluna/dailyselfie/notification/AlarmBroadcastReceiver.java
+++ app/src/main/java/com/github/jgluna/dailyselfie/notification/AlarmBroadcastReceiver.java
@@ -1,18 +1,29 @@
 package com.github.jgluna.dailyselfie.notification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.util.Log;
 
 import com.github.jgluna.dailyselfie.MainActivity;
+import com.github.jgluna.dailyselfie.R;
 
-public class AlarmBroadcastReceiver extends BroadcastReceiver{
+public class AlarmBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.v("RECEIVER", "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         Intent activityIntent = new Intent(context, MainActivity.class);
         activityIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        context.startActivity(activityIntent);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, activityIntent, 0);
+        Notification notification = new Notification.Builder(context)
+                .setContentTitle("Daily Selfie")
+                .setContentText("Time to take your selfie!!")
+                .setSmallIcon(R.mipmap.ic_launcher).setContentIntent(pendingIntent)
+                .build();
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notification.flags |= Notification.FLAG_AUTO_CANCEL;
+        manager.notify(0, notification);
     }
+
 }

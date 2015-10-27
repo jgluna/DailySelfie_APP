d2665111744fdb6a2b579960bf75a2974a7bb79a
Juan Luna <jgluna85@gmail.com>
2015-10-27 14:03:34 -0500

    Added retrofit Added call to WS

--- app/src/main/java/com/github/jgluna/dailyselfie/comm/BackgroundTask.java
+++ app/src/main/java/com/github/jgluna/dailyselfie/comm/BackgroundTask.java
@@ -1,17 +1,35 @@
 package com.github.jgluna.dailyselfie.comm;
 
 import android.os.AsyncTask;
+import android.util.Log;
 
 import com.github.jgluna.dailyselfie.model.EffectsRequestWrapper;
 
-public class BackgroundTask extends AsyncTask<EffectsRequestWrapper,Void,String> {
+import retrofit.Callback;
+import retrofit.RestAdapter;
+import retrofit.RetrofitError;
+import retrofit.client.Response;
+
+public class BackgroundTask extends AsyncTask<EffectsRequestWrapper, Void, String> {
     @Override
     protected String doInBackground(EffectsRequestWrapper... params) {
+        RestAdapter restAdapter = new RestAdapter.Builder().setLogLevel(RestAdapter.LogLevel.FULL).setEndpoint("http://localhost:8080/").build();
+        EffectsControllerInterface restService = restAdapter.create(EffectsControllerInterface.class);
+        //TODO asignar imagen a parametro 1
+        restService.applyEffect(null, params[0], new Callback<Response>() {
+            @Override
+            public void success(Response response, Response response2) {
+                //TODO modificar la imagen enviada por la obtenida
+                Log.v("BackgroundTask", "yay!!");
+            }
+
+            @Override
+            public void failure(RetrofitError error) {
+                //TODO toast de error
+                Log.v("BackgroundTask", "buu :(");
+            }
+        });
         return null;
     }
-
-    @Override
-    protected void onPostExecute(String s) {
-        super.onPostExecute(s);
-    }
+//TODO deberia sobreescribir postExecute? quizas para marcar las imagenes como actualizando?
 }
